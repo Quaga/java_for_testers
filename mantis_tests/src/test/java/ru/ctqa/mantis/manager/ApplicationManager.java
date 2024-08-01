ba11025b80cf51b6152a192c536955a448448088
Quaga <mmanapov@gmail.com>
2024-08-02 00:10:11 +0300

    Задание №17: Подготовить новый проект для нового учебного приложения

---
+++ mantis_tests/src/test/java/ru/ctqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,35 @@
+package ru.ctqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+    private WebDriver driver;
+    private String browser;
+    private Properties properties;
+
+    public void init(String browser, Properties properties) {
+        this.browser = browser;
+        this.properties = properties;
+    }
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseUrl"));
+            driver.manage().window().setSize(new Dimension(1050, 790));
+        }
+        return driver;
+    }
+}
